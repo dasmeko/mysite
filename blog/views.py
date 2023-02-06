bac9d2ada2fafbca02cd13d733bd6a418def6b9e
Dasha <dashadadaday@gmail.com>
2023-02-06 19:49:57 +0200

    Unified new post and edit post templates

--- blog/views.py
+++ blog/views.py
@@ -12,23 +12,20 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+def post_new_or_edit(request, **kwargs):
+    if len(kwargs):
+        edit = True
+        pk = kwargs['pk']
+        post = get_object_or_404(Post, pk=pk)
     else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+        edit = False
+    
+    if request.method == "POST":
+        if edit:
+            form = PostForm(request.POST, instance=post)
+        else:
+            form = PostForm(request.POST)
 
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
@@ -36,5 +33,10 @@ def post_edit(request, pk):
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm(instance=post)
+        if edit:
+            form = PostForm(instance=post)
+        else:
+            form = PostForm()
+    
     return render(request, 'blog/post_edit.html', {'form': form})
+    
